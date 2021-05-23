039e0c5d16abbe0cb4a4c35d998355562b894ffa
sharker <bigworld51931@126.com>
2021-05-24 02:20:22 +0800

    1.RoundRectProgressBar优化(二)

--- app/src/main/java/com/wolfsea/defineviewgroupdemo/RoundRectProgressBar.kt
+++ app/src/main/java/com/wolfsea/defineviewgroupdemo/RoundRectProgressBar.kt
@@ -3,6 +3,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
 /**
@@ -28,8 +29,11 @@ class RoundRectProgressBar(context: Context, attributeSet: AttributeSet) : View(
 
     private var pieces = 0F
 
+    private var rx = 0f
+    private var ry = 0f
+
     init {
-        init()
+        init(context, attributeSet)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -39,12 +43,14 @@ class RoundRectProgressBar(context: Context, attributeSet: AttributeSet) : View(
         when (MeasureSpec.getMode(widthMeasureSpec)) {
             MeasureSpec.EXACTLY -> {
 
-                progressWidth = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight
+                progressWidth = if (progressWidth == 0) MeasureSpec.getSize(widthMeasureSpec) else progressWidth
+                progressWidth = progressWidth - paddingLeft - paddingRight
             }
             MeasureSpec.AT_MOST -> {
 
                 val defaultWidth = resources.getDimension(R.dimen.dp_100).toInt()
-                progressWidth = defaultWidth.coerceAtMost(MeasureSpec.getSize(widthMeasureSpec)) - paddingLeft - paddingRight
+                progressWidth =
+                    defaultWidth.coerceAtMost(MeasureSpec.getSize(widthMeasureSpec)) - paddingLeft - paddingRight
             }
             MeasureSpec.UNSPECIFIED -> {}
             else -> {}
@@ -53,12 +59,14 @@ class RoundRectProgressBar(context: Context, attributeSet: AttributeSet) : View(
         when (MeasureSpec.getMode(heightMeasureSpec)) {
             MeasureSpec.EXACTLY -> {
 
-                progressHeight = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom
+                progressHeight = MeasureSpec.getSize(heightMeasureSpec)
+                progressHeight = progressHeight - paddingTop - paddingBottom
             }
             MeasureSpec.AT_MOST -> {
 
-                val defaultHeight = resources.getDimension(R.dimen.dp_25).toInt()
-                progressHeight = defaultHeight.coerceAtMost(MeasureSpec.getSize(widthMeasureSpec)) - paddingLeft - paddingRight
+                val defaultHeight = if (progressHeight == 0) MeasureSpec.getSize(heightMeasureSpec) else progressHeight
+                progressHeight =
+                    defaultHeight.coerceAtMost(MeasureSpec.getSize(widthMeasureSpec)) - paddingTop - paddingBottom
             }
             MeasureSpec.UNSPECIFIED -> {}
             else -> {}
@@ -74,14 +82,14 @@ class RoundRectProgressBar(context: Context, attributeSet: AttributeSet) : View(
             save()
             
             backgroundRectF.set(0F, 0F, progressWidth.toFloat(), progressHeight.toFloat())
-            drawRoundRect(backgroundRectF, RX, RY, backgroundPaint)
+            drawRoundRect(backgroundRectF, rx, ry, backgroundPaint)
 
             progressRectF.set(0F, 0F, currentProgress.times(pieces), progressHeight.toFloat())
             if (!currentProgress.equals(TOTAL_PIECES)) {
 
-                clipRect(0F, 0F, currentProgress.times(pieces).minus(RX), progressHeight.toFloat())
+                clipRect(0F, 0F, currentProgress.times(pieces).minus(rx), progressHeight.toFloat())
             }
-            drawRoundRect(progressRectF, RX, RY, progressPaint)
+            drawRoundRect(progressRectF, rx, ry, progressPaint)
 
             restore()
         }
@@ -93,7 +101,7 @@ class RoundRectProgressBar(context: Context, attributeSet: AttributeSet) : View(
     }
 
     //初始化方法
-    private fun init() {
+    private fun init(context: Context, attributeSet: AttributeSet) {
 
         backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG)
         backgroundPaint.color = resources.getColor(R.color.teal_200)
@@ -117,12 +125,24 @@ class RoundRectProgressBar(context: Context, attributeSet: AttributeSet) : View(
             Shader.TileMode.CLAMP
         )
 
+        context.obtainStyledAttributes(attributeSet, R.styleable.RoundRectProgressBar)
+            .apply {
+
+                progressWidth =
+                    getDimension(R.styleable.RoundRectProgressBar_roundBarWidth, 0F).toInt()
+                progressHeight =
+                    getDimension(R.styleable.RoundRectProgressBar_roundBarHeight, 0F).toInt()
+
+                rx = getDimension(R.styleable.RoundRectProgressBar_roundBarRx, 20F)
+                ry = getDimension(R.styleable.RoundRectProgressBar_roundBarRx, 20F)
+
+                recycle()
+            }
+
         progressPaint.shader = progressGradientDrawable
     }
 
     companion object {
-        const val RX = 15F
-        const val RY = 15F
         const val TOTAL_PIECES = 100F
     }
 
